b345585916b677ba37a17dacc62416ef4998999c
Marcin Głąb <anrok1234@gmail.com>
2019-11-07 13:43:57 +0100

    - detailed inquiries

--- spring-data-boot/src/main/java/springbootdata/app/SpringDataBoot.java
+++ spring-data-boot/src/main/java/springbootdata/app/SpringDataBoot.java
@@ -17,11 +17,36 @@ public class SpringDataBoot {
 
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("A4", "Audi", 49000.0));
+        cars.add(new Car("A5", "Audi", 67000.0));
         cars.add(new Car("Auris", "Toyota", 35000.0));
         cars.add(new Car("Insignia", "Opel", 29500.0));
+        cars.add(new Car("A8", "Audi", 28000.0));
+        cars.add(new Car("Corolla", "Toyota", 31000.0));
+        cars.add(new Car("Vectra", "Opel", 29500.0));
+        cars.add(new Car("Astra", "Opel", 29500.0));
 
         CarRepository carRepo = ctx.getBean(CarRepository.class);
         cars.forEach(carRepo::save); //zapisujemy samochody
+
+        System.out.println("Toyota cars:");
+        List<Car> toyotaCars = carRepo.findByBrand("Toyota");
+        toyotaCars.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("First Opel Car:");
+        Car firstOpel = carRepo.findFirstByBrand("Opel");
+        System.out.println(firstOpel + "\n");
+
+        System.out.println("Audi for 49000: ");
+        carRepo.findAllByBrandAndPrice("Audi", 49000).forEach(System.out::println);
+        System.out.println("Toyota or Opel:");
+        carRepo.findAllByBrandOrBrand("Toyota", "Opel").forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("All Audi cars, price ascending");
+        carRepo.findAllByBrandOrderByPriceAsc("Audi")
+                .forEach(System.out::println);
+        System.out.println();
 
         Car firstCar = carRepo.findById(1L).get(); //pobieramy pierwszy
         carRepo.delete(firstCar); //usuwamy go
