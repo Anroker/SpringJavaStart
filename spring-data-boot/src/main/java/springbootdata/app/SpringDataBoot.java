73eb6c70a2956ccce8e9e01fc0293096a26eb4ec
Marcin Głąb <anrok1234@gmail.com>
2019-11-08 13:03:36 +0100

    - Querry addnotation

--- spring-data-boot/src/main/java/springbootdata/app/SpringDataBoot.java
+++ spring-data-boot/src/main/java/springbootdata/app/SpringDataBoot.java
@@ -3,11 +3,10 @@ package springbootdata.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import springbootdata.app.model.Car;
-import springbootdata.app.repository.CarRepository;
+import springbootdata.app.model.Employee;
+import springbootdata.app.repository.EmployeeRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringDataBoot {
@@ -15,7 +14,24 @@ public class SpringDataBoot {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(SpringDataBoot.class);
 
-        List<Car> cars = new ArrayList<>();
+        EmployeeRepository repository = ctx.getBean(EmployeeRepository.class);
+
+        Stream.of(
+                new Employee("Wojtek", "Kowalski", 3000.0),
+                new Employee("Barnabasz", "Rabarbar", 4000.0),
+                new Employee("Rumcajs", "styczka", 5000.0),
+                new Employee("Ratatator", "Rotototor", 6000.0)
+        ).forEach(repository::save);
+
+        System.out.println("Employees with last name Kowalski and salary >=2000");
+        repository.findByLastNameAndSalary("Kowalski", 2000).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Employee names with salary >= 4000");
+        repository.namesForSalaryAsc(4000)
+                .forEach(System.out::println);
+
+        /*List<Car> cars = new ArrayList<>();
         cars.add(new Car("A4", "Audi", 49000.0));
         cars.add(new Car("A5", "Audi", 67000.0));
         cars.add(new Car("Auris", "Toyota", 35000.0));
@@ -52,7 +68,7 @@ public class SpringDataBoot {
         carRepo.delete(firstCar); //usuwamy go
 
         //pobieramy i wyświetlamy pozostałe
-        carRepo.findAll().forEach(System.out::println);
+        carRepo.findAll().forEach(System.out::println);*/
 
         ctx.close();
     }
